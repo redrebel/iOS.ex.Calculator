3243e54c62beb7318d28aacadfd13eb02d7872b0
Lee, Jung Geun <cjred77@gmail.com>
2018-01-10 21:08:48 +0900

    Initial Commit

---
+++ exmp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exmp
+//
+//  Created by 이정근 on 2018. 1. 10..
+//  Copyright © 2018년 이정근. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
